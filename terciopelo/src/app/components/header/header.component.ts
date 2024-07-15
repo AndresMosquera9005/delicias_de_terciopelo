b09e193d8852e2bfdc9d61ed58d4d37720af8706
AndresMosquera9005 <andrubjj@gmail.com>
2024-07-15 16:10:39 -0500

    se modifico: body, header y saludables

--- terciopelo/src/app/components/header/header.component.ts
+++ terciopelo/src/app/components/header/header.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { SaludablesComponent } from '../saludables/saludables.component';
+import { TradicionalesComponent } from '../tradicionales/tradicionales.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [SaludablesComponent, TradicionalesComponent],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  constructor( private router: Router ){  }
+
+  listaMenuSaludable(){
+    this.router.navigate(['saludables'])
+  }
+
+  listaMenuTradicionales(){
+    this.router.navigate(['tradicionales'])
+  }
 
 }
--- terciopelo/src/app/components/saludables/saludables.component.ts
+++ terciopelo/src/app/components/saludables/saludables.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
 
 @Component({
   selector: 'app-saludables',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './saludables.component.html',
   styleUrl: './saludables.component.css'
 })
