1a0389938a6a7848f589dea4f000fbb6acca5166
AndresMosquera9005 <andrubjj@gmail.com>
2024-07-25 17:52:43 -0500

    se agregaron dos fotos de ensayo, se agrego components lo-mas-vendido y se modificaron

--- terciopelo/src/app/components/body/body.component.ts
+++ terciopelo/src/app/components/body/body.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { QuienesSomosComponent } from "../quienes-somos/quienes-somos.component";
 import { RedesSocialesComponent } from "../redes-sociales/redes-sociales.component";
+import { LoMasVendidoComponent } from "../lo-mas-vendido/lo-mas-vendido.component";
 
 @Component({
   selector: 'app-body',
   standalone: true,
-  imports: [HeaderComponent, QuienesSomosComponent, RedesSocialesComponent],
+  imports: [HeaderComponent, QuienesSomosComponent, RedesSocialesComponent, LoMasVendidoComponent],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css'
 })
---
+++ terciopelo/src/app/components/lo-mas-vendido/lo-mas-vendido.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lo-mas-vendido',
+  standalone: true,
+  imports: [],
+  templateUrl: './lo-mas-vendido.component.html',
+  styleUrl: './lo-mas-vendido.component.css'
+})
+export class LoMasVendidoComponent {
+
+}
