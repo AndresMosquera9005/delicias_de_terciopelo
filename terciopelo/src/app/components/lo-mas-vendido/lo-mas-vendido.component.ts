4a4d396e957d22d5073e83755d385cc7b912ef32
AndresMosquera9005 <andrubjj@gmail.com>
2024-07-29 15:14:28 -0500

    se modificolos tres archivos lo-mas-vendidio

--- terciopelo/src/app/components/lo-mas-vendido/lo-mas-vendido.component.ts
+++ terciopelo/src/app/components/lo-mas-vendido/lo-mas-vendido.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-lo-mas-vendido',
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './lo-mas-vendido.component.css'
 })
 export class LoMasVendidoComponent {
+  @Input() tarjeta: any;
 
 }
