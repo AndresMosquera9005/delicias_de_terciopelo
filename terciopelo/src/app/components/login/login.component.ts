7b455bf13a6ce5b35e512b026a017d6ae29e82f7
AndresMosquera9005 <andrubjj@gmail.com>
2024-08-02 18:02:00 -0500

    se modifico login. css, login.html, login.ts

--- terciopelo/src/app/components/login/login.component.ts
+++ terciopelo/src/app/components/login/login.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { RegistroComponent } from '../registro/registro.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [RegistroComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  constructor( private router: Router ){  }
+
+
+  registro(){
+    this.router.navigate(['registro'])
+  }
 
 }
